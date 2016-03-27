bef1479e6cc1e77ec4e3d624af69720efab6a304
lilei8630 <lilei_8630@163.com>
2016-03-27 16:05:06 +0800

    3.27

--- Rank_Scores.sql
+++ Rank_Scores.sql
@@ -10,3 +10,10 @@ SELECT Scores.*
 FROM (SELECT @counter:=0) v,Scores
 HAVING (@counter :=@counter +1) =3
 ORDER BY Id;
+//计算累加和
+SELECT t.Id,
+       t.Score,
+       @x = @x+t.Score as Total
+FROM
+       (SELECT @x := 0) a,
+       Scores t;
