c558f4ba5eec296c2996034c95a9480712eade51
lilei8630 <lilei_8630@163.com>
2016-03-27 15:50:13 +0800

    3.27

--- Rank_Scores.sql
+++ Rank_Scores.sql
@@ -5,3 +5,8 @@ SELECT
 FROM    
         (SELECT @counter:=0) v,
         Scores;
+//只抽取一个表的第三行
+SELECT Scores.*
+FROM (SELECT @counter:=0) v,Scores
+HAVING (@counter :=@counter +1) %3 =0
+ORDER BY Id;
