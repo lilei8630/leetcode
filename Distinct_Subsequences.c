cf607747fc8e1c762de84362577f3cde9fe5f242
lilei8630 <lilei_8630@163.com>
2016-04-02 14:45:37 +0800

    4.2

--- Distinct_Subsequences.c
+++ Distinct_Subsequences.c
@@ -6,22 +6,30 @@ int numDistinct(char * s,char * t){
     if(lens==0&&lent==0) return 1;
     if(lent>lens) return 0;
     int c[lent+1][lens+1];
-    
     for(int i = 0 ; i<=lens;i++) c[0][i] = 1;
     for(int i = 1 ; i<=lent;i++) c[i][0] = 0;
     
     for(int i=1;i<=lent;i++){
         for(int j = 1;j<=lens;j++){
             c[i][j] = c[i][j-1];
-            if(s[j]==t[i]) c[i][j] +=c[i-1][j-1];
+            if(s[j-1]==t[i-1]) {
+                c[i][j] +=c[i-1][j-1];
+            }
         }
     }
+    //for(int i =0;i<=lens;i++){
+      //  for(int j=0;j<=lent;j++){
+        //    printf("%d ",c[i][j]);
+
+        //}
+       // printf("\n");
+   // }
     return c[lent][lens];
 }
 
 int  main(){
    
-    printf("%d\n", numDistinct("bccbcdcabadabddbccaddcbabbaaacdba","bccbbdc"));
+    printf("%d\n", numDistinct("rabbbit","rabbit"));
     return 0;
     
 }
